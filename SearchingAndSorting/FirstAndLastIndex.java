5c007993630bcf478aeba04f39b426748c7b5d8d
Imran Khan <imrankhan96799@gmail.com>
2022-12-23 21:06:52 +0530

    first index and last index using recursion.And bayer moore vorting algorithm

--- SearchingAndSorting/FirstAndLastIndex.java
+++ SearchingAndSorting/FirstAndLastIndex.java
@@ -25,59 +25,112 @@ Note: If the number x is not found in the array just return both index as -1.
 
 package SearchingAndSorting;
 
-
 public class FirstAndLastIndex {
     public static void main(String[] args) {
-        int[] arr ={ 1, 3, 5, 5, 5, 5, 7, 123, 125 };
-        int key = 7;
-        int[] fl =firstAndLastIndex(arr,key);
-        System.out.println("The first index is: "+fl[0]+" and the last index is: "+fl[1]);
+        int[] arr = { 1, 3, 5, 5, 5, 5, 7, 123, 125 };
+        int key = 5;
+        int[] fl = firstAndLastIndex(arr, key);
+        System.out.println("The first index is: " + fl[0] + " and the last index is: " + fl[1]);
+        int fir = firstIndexRecursion(arr, 0, arr.length, key);
+        if (fir == -1)
+            System.out.println("Target not present");
+        else
+            System.out.println("The first index of element is " + fir);
+
+        int lir = lastIndexRecursion(arr, 0, arr.length, key);
+        if (lir == -1)
+            System.out.println("Target not present");
+        else
+            System.out.println("The last index of element is " + lir);
+
     }
-    public static int[] firstAndLastIndex(int[] arr,int key){
+
+    public static int[] firstAndLastIndex(int[] arr, int key) {
         int[] fAndL = new int[2];
-        
 
         int first = firstIndex(arr, key);
         int last = lastIndex(arr, key);
-         fAndL[0] = first;
-         fAndL[1] = last;
+        fAndL[0] = first;
+        fAndL[1] = last;
 
         return fAndL;
     }
-    private static int firstIndex(int[] arr,int key){
-            int first = -1;
-            int start = 0;
-            int end = arr.length - 1;
-            while(start <= end ){
-                int mid = start +(end -start )/2;
-                if(arr[mid] == key){
-                    first = mid;
-                    end = mid - 1;
 
-                }else if(arr[mid] < key){
-                    start = mid + 1;
-                }else{
-                    end = mid - 1 ;
-                }
+    private static int firstIndex(int[] arr, int key) {
+        int first = -1;
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == key) {
+                first = mid;
+                end = mid - 1;
+
+            } else if (arr[mid] < key) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
             }
-            return first;
+        }
+        return first;
     }
-    private static int lastIndex(int[] arr, int key){
+
+    private static int lastIndex(int[] arr, int key) {
         int last = -1;
         int start = 0;
-        int end = arr.length -1 ;
-        while(start <= end ){
-            int mid = start + (end - start)/2;
-            if(arr[mid] == key){
-                last = mid ;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == key) {
+                last = mid;
                 start = mid + 1;
-            } else if(arr[mid] < key){
+            } else if (arr[mid] < key) {
                 start = mid + 1;
-            }else{
-                end = mid - 1; 
+            } else {
+                end = mid - 1;
             }
 
         }
         return last;
+    }
+
+    // Using recursion
+    private static int firstIndexRecursion(int[] arr, int start, int end, int key) {
+        // base case
+        if (start > end)
+            return -1;
+        int mid = start + (end - start) / 2;
+        if (arr[mid] == key) {
+            int fiisa = firstIndexRecursion(arr, start, mid - 1, key);
+            if (fiisa != -1)
+                return fiisa;
+            else
+                return mid;
+
+        } else if (arr[mid] < key)
+            return firstIndexRecursion(arr, mid + 1, end, key);
+        else
+            return firstIndexRecursion(arr, start, mid - 1, key);
+    }
+
+    private static int lastIndexRecursion(int[] arr, int start, int end, int key) {
+        // base case
+        if (start > end)
+            return -1;
+
+        int mid = start + (end - start) / 2;
+        if (arr[mid] == key) {
+
+            int lisa = lastIndexRecursion(arr, mid + 1, end, key);
+            if (lisa != -1)
+                return lastIndexRecursion(arr, mid + 1, end, key);
+            else
+                return mid;
+
+        } else if (arr[mid] < key)
+            return lastIndexRecursion(arr, mid + 1, end, key);
+        else
+            return lastIndexRecursion(arr, start, mid - 1, key);
+
     }
 }
--- SearchingAndSorting/MajorityElement.java
+++ SearchingAndSorting/MajorityElement.java
@@ -33,64 +33,116 @@ The task is to complete the function majorityElement() which returns the majorit
 package SearchingAndSorting;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class MajorityElement {
- public static void main(String[] args) {
-    int[] arr = {1,2,3};
-    int size = arr.length;
-    int majorityElement = majorityElement(arr, size);
-    System.out.println(majorityElement!=-1?"The majoroty element is "+majorityElement:"No majority elements exists");
- }
- public static int majorityElement(int[] arr ,int size){
-    Arrays.sort(arr);
-    for (int i = 0; i < size; i++) {
-        if(majorityExists(arr,size,arr[i])){
-            return arr[i];
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int size = input.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter the array elements : ");
+        for (int i = 0; i < size; i++) {
+            arr[i] = input.nextInt();
+        }
+        input.close();
+        int majorityElement = majorityElement(arr, size);
+        System.out.println(
+                majorityElement != -1 ? "The majority element is " + majorityElement : "No majority elements exists");
+
+        int ans = majorityElement(arr);
+        if (ans != -1) {
+            System.out.println("The majority element is " + ans);
+
+        } else {
+            System.out.println("No majority element exists ");
         }
     }
-    return -1;
- }
- private static boolean majorityExists(int[] arr,int size , int key){
-    int first = firstIndex(arr,size,key);
-    int last = lastIndex(arr, size, key);
-    int totalOccurance = last - first + 1;
-    if(totalOccurance > Math.floor(size/2)){
-        return true;
-    }
-    return false;
- }
- private static int firstIndex(int[] arr, int size,int key){
-    int start = 0;
-    int end = size - 1;
-    int ans = -1;
-    while(start<=end){
-        int mid = start + (end -start)/2;
-        if(arr[mid]==key){
-            ans = mid;
-            end = mid - 1;
-        }else if(arr[mid]<key){
-            start = mid+1;
-        }else{
-            end = mid  - 1;
+
+    public static int majorityElement(int[] arr, int size) {
+        Arrays.sort(arr);
+        for (int i = 0; i < size; i++) {
+            if (majorityExists(arr, size, arr[i])) {
+                return arr[i];
+            }
         }
+        return -1;
     }
-    return ans;
- }
- private static int lastIndex(int[] arr, int size,int key){
-    int start = 0;
-    int end = size - 1;
-    int ans = -1;
-    while(start<=end){
-        int mid = start + (end -start)/2;
-        if(arr[mid]==key){
-            ans = mid;
-            start = mid +1;
-        }else if(arr[mid]<key){
-            start = mid+1;
-        }else{
-            end = mid  - 1;
+
+    private static boolean majorityExists(int[] arr, int size, int key) {
+        int first = firstIndex(arr, size, key);
+        int last = lastIndex(arr, size, key);
+        int totalOccurance = last - first + 1;
+        if (totalOccurance > Math.floor(size / 2)) {
+            return true;
         }
+        return false;
     }
-    return ans;
- }
+
+    private static int firstIndex(int[] arr, int size, int key) {
+        int start = 0;
+        int end = size - 1;
+        int ans = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == key) {
+                ans = mid;
+                end = mid - 1;
+            } else if (arr[mid] < key) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    private static int lastIndex(int[] arr, int size, int key) {
+        int start = 0;
+        int end = size - 1;
+        int ans = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == key) {
+                ans = mid;
+                start = mid + 1;
+            } else if (arr[mid] < key) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    // Bayer Moore Voting Algorithm
+    private static int majorityElement(int[] arr) {
+        int count = 0;
+        int ele = -1;
+        for (int i = 0; i < arr.length; i++) {
+
+            if (count == 0) {
+                ele = arr[i];
+                count = 1;
+            } else {
+                if (ele == arr[i]) {
+                    count++;
+                } else {
+                    count--;
+                }
+            }
+
+        }
+        if (count > 0) {
+            int counter = 0;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == ele) {
+                    counter++;
+                }
+            }
+            if (counter >= Math.floor(arr.length / 2))
+                return ele;
+        }
+        return -1;
+    }
 }
